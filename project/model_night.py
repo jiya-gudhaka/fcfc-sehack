30710112dc2b419492fabed4050388673f9b6f9a
Jiya Gudhaka <jiya.gudhaka23@spit.ac.in>
2025-04-20 07:58:11 +0530

    nighr modified

--- project/model_night.py
+++ project/model_night.py
@@ -7,7 +7,7 @@ def process_frame(frame):
     results = model(frame)
     alert = None
     for box in results[0].boxes:
-        if model.names[int(box.cls[0])].lower() == "person":
+        if model.names[int(box.cls[0])].lower() == "people":
             alert = "Person detected at night!"
     return results[0].plot(), alert
 
